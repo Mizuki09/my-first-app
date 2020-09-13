9c1a4d348af62fda432383f553e64ccf7791e31f
kaniqq <karukaniq@gmail.com>
2020-09-13 23:04:27 +0900

    commit

--- app/app/Video.php
+++ app/app/Video.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Video extends Model
 {
+    
     protected $guarded = array('id');
     protected $table = 'videos';
     protected $primaryKey = 'id';
