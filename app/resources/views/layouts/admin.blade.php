d71cd4b6fef701516ea1c4f26007f6b244c9e4c4
kaniqq <karukaniq@gmail.com>
2020-09-20 08:58:42 +0900

    インデント修正

--- app/resources/views/layouts/admin.blade.php
+++ app/resources/views/layouts/admin.blade.php
@@ -1,83 +1,88 @@
 <!DOCTYPE html>
 <html lang=ja>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title')</title>
-{{--@yieldにタイトルを入れる--}}
+        <title>@yield('title')</title>
+    {{--@yieldにタイトルを入れる--}}
 
-<!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+    <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
-</head>
-<body>
-<div id="app">
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div id="app">
 
-    <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
-        <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('TOP', 'TOP') }}
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+            <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
+                <div class="container">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        {{ config('TOP', 'TOP') }}
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
-                    @guest
-                        <li><a class="nav-link" href="{{ route('login') }}">{{ __('ログイン') }}</a></li>
-                        {{-- ログインしていたらユーザー名とログアウトボタンを表示 --}}
-                        <li><a class="nav-link" href="{{ route('register') }}">{{ __('新規登録') }}</a></li>
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav mr-auto">
+                            @guest
+                                <li><a class="nav-link" href="{{ route('login') }}">{{ __('ログイン') }}</a></li>
+                                {{-- ログインしていたらユーザー名とログアウトボタンを表示 --}}
+                                <li><a class="nav-link" href="{{ route('register') }}">{{ __('新規登録') }}</a></li>
+                            @else
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ Auth::user()->name }} <span class="caret"></span>
+                                    </a>
 
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ url("edit".'/'.Auth::user()->id) }}">
-                                    {{ __('登録情報の編集') }}
-                                </a>
-                                <a class="dropdown-item" href="{{ url("video".'/'.Auth::user()->id) }}">
-                                    {{ __('投稿動画の管理') }}
-                                </a>
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                             document.getElementById('logout-form').submit();">
-                                    {{ __('ログアウト') }}
-                                </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        @if(Auth::user()->role == "admin")
+                                            <a class="dropdown-item" href="{{ url("admin") }}">
+                                                {{ __('ユーザー管理') }}
+                                            </a>
+                                        @endif
+                                        <a class="dropdown-item" href="{{ url("edit".'/'.Auth::user()->id) }}">
+                                            {{ __('登録情報の編集') }}
+                                        </a>
+                                        <a class="dropdown-item" href="{{ url("video".'/'.Auth::user()->id) }}">
+                                            {{ __('投稿動画の管理') }}
+                                        </a>
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                           onclick="event.preventDefault();
+                                                                     document.getElementById('logout-form').submit();">
+                                            {{ __('ログアウト') }}
+                                        </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            @csrf
+                                        </form>
 
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
+                                    </div>
+                                </li>
+                            @endguest
+                        </ul>
 
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-                </ul>
-            </div>
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ml-auto">
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+
+            <main class="py-4">
+                @yield('content')
+            </main>
         </div>
-    </nav>
-
-
-    <main class="py-4">
-        @yield('content')
-    </main>
-</div>
-</body>
+    </body>
 </html>
